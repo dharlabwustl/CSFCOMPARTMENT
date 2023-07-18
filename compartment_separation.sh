cea9408c3d36f31972b5ed35f620678ed6035af3
dharlabwustl <atulkumar@wustl.edu>
2023-07-18 11:22:28 -0500

    First Commit

--- compartment_separation.sh
+++ compartment_separation.sh
@@ -4,4 +4,3 @@ echo " I AM RUNNING "
 
 # single filename NECT, its CSF mask and other relevant files
 python /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/CSFCOMPARTMENT/CSF_COMPARTMENT_v1_part2.py
-
