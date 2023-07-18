501ab8b46693d3f9d7962f8ba7e937842172760f
dharlabwustl <atulkumar@wustl.edu>
2023-07-18 14:04:44 -0500

    First Commit

--- Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
+++ Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
@@ -36,18 +36,17 @@ from skimage import exposure
 from scipy import ndimage
 from skimage.morphology import skeletonize
 from skimage.util import invert
-# sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts/imagedisplay')
-# sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/MIDLINE/SOFTWARE/pyscripts/csfbased')
-# sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts')
-# sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/HEMORRHAGE/SOFTWARE/pyScripts')
-# from utilities import *
-# from vtk_python_functions import *
-# from csf_bounding_box import *
-from utilities_simple_trimmed import *
+sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts/imagedisplay')
+sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/MIDLINE/SOFTWARE/pyscripts/csfbased')
+sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts')
+sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/HEMORRHAGE/SOFTWARE/pyScripts')
+from utilities import *
+from vtk_python_functions import *
+from csf_bounding_box import *
 from matplotlib.patches import Circle
 import subprocess
 import cv2
-# from savenumpymatrix import *
+from savenumpymatrix import *
 # identify the slice which contains the
 import vtk
 import itk
@@ -60,14 +59,14 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from matplotlib import cm
 import sys, subprocess
-# from volume_rendering_vtk import *
+from volume_rendering_vtk import *
 from vtk import * 
 from vtk import (
     vtkJPEGReader, vtkImageCanvasSource2D, vtkImageActor, vtkPolyDataMapper,
     vtkRenderer, vtkRenderWindow, vtkRenderWindowInteractor, vtkSuperquadricSource,
     vtkActor, VTK_MAJOR_VERSION
 )
-# from myfunctions import *
+from myfunctions import *
 import re
 colors = vtk.vtkNamedColors()
 renderer = vtk.vtkRenderer()
