6bbfeb3288777e8937f6bd8dbb7a50ba37253b6c
dharlabwustl <atulkumar@wustl.edu>
2023-07-18 12:16:41 -0500

    First Commit

--- CSF_COMPARTMENT_v1_part2_July18_2023.py
+++ CSF_COMPARTMENT_v1_part2_July18_2023.py
@@ -47,6 +47,10 @@ def csf_compartments(filename_gray,filename_mask,filename_bet):
     returnvalue=0
     try:
         latexfilename=os.path.join(os.path.dirname(SLICE_OUTPUT_DIRECTORY),os.path.basename(filename_gray)+".tex")
+        # allgrayscalefolder = os.listdir(NECT_directory_name_parent)
+        file1=latex_start(latexfilename)
+        latex_start(latexfilename)
+        latex_begin_document(latexfilename)
         print("filename_gray")
         print(filename_gray)
         sulci_vol, ventricle_vol,leftcountven,rightcountven,leftcountsul,rightcountsul,sulci_vol_above_vent,sulci_vol_below_vent,sulci_vol_at_vent = divideintozones_v1(latexfilename,SLICE_OUTPUT_DIRECTORY,filename_gray,filename_mask,filename_bet)
