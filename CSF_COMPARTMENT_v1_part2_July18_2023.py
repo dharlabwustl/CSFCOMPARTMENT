dc5a0b34c31cf366db46dabf72117207b158e7a5
dharlabwustl <atulkumar@wustl.edu>
2023-07-18 12:50:23 -0500

    First Commit

--- CSF_COMPARTMENT_v1_part2_July18_2023.py
+++ CSF_COMPARTMENT_v1_part2_July18_2023.py
@@ -8,14 +8,14 @@ Created on Thu Sep  5 10:26:44 2019
 """
 import os.path
 import sys,argparse,inspect
-sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts')
+# sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts')
 
 from sympy import *
 
-sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts/imagedisplay')
-sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/MIDLINE/SOFTWARE/pyscripts/csfbased')
-sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts')
-sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/HEMORRHAGE/SOFTWARE/pyScripts')
+# sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts/imagedisplay')
+# sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/MIDLINE/SOFTWARE/pyscripts/csfbased')
+# sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/CommonPrograms/pyscripts')
+# sys.path.append('/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/HEMORRHAGE/SOFTWARE/pyScripts')
 from utilities_master import *
 
 from  Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019 import * 
@@ -46,7 +46,7 @@ betsuffix="_levelset_bet"
 def csf_compartments(filename_gray,filename_mask,filename_bet):
     returnvalue=0
     try:
-        latexfilename=os.path.join(os.path.dirname(SLICE_OUTPUT_DIRECTORY),os.path.basename(filename_gray).split('.nii')[0].replace('.','_')+".tex")
+        latexfilename=os.path.join(os.path.dirname(SLICE_OUTPUT_DIRECTORY),ANAYLYSIS_TYPE+".tex")
         sulci_vol, ventricle_vol,leftcountven,rightcountven,leftcountsul,rightcountsul,sulci_vol_above_vent,sulci_vol_below_vent,sulci_vol_at_vent = divideintozones_v1(latexfilename,SLICE_OUTPUT_DIRECTORY,filename_gray,filename_mask,filename_bet)
         latex_start_table2c(latexfilename)
         latex_inserttext_table2c(latexfilename,text1='SulciVol:', text2=str(sulci_vol))
