5d0e017591362d63857869662c1e9d72ecfb099c
dharlabwustl <atulkumar@wustl.edu>
2023-07-18 11:55:35 -0500

    First Commit

--- CSF_COMPARTMENT_v1_part2_July18_2023.py
+++ CSF_COMPARTMENT_v1_part2_July18_2023.py
@@ -77,6 +77,7 @@ def csf_compartments(filename_gray,filename_mask,filename_bet):
 
         write_csv(csvfile_with_vol,csv_columns,dict_for_csv)
         latex_end(latexfilename)
+        latex_file_build(latexfilename)
         print("I SUCCEED AT ::{}".format(inspect.stack()[0][3]))
         return 1
     except:
