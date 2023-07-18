ddb7903997e4cbb7bd9e7ec87557ff8f814a4a1c
dharlabwustl <atulkumar@wustl.edu>
2023-07-18 12:19:07 -0500

    First Commit

--- CSF_COMPARTMENT_v1_part2_July18_2023.py
+++ CSF_COMPARTMENT_v1_part2_July18_2023.py
@@ -76,7 +76,7 @@ def csf_compartments(filename_gray,filename_mask,filename_bet):
         latex_end(latexfilename)
         latex_file_build(latexfilename)
         print("I SUCCEED AT ::{}".format(inspect.stack()[0][3]))
-        return 1
+        returnvalue=1
     except:
         print("I FAILED AT ::{}".format(inspect.stack()[0][3]))
         pass
