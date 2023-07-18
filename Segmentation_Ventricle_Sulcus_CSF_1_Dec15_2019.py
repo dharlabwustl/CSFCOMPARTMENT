2f66aca8d4291af7589bda0cf58a3f16546298fe
dharlabwustl <atulkumar@wustl.edu>
2023-07-18 15:58:09 -0500

    First Commit

--- Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
+++ Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
@@ -157,7 +157,7 @@ def divideintozones_v1(filename_gray,filename_mask,filename_bet):
 
                 csf_ids.append([l,stats.GetPhysicalSize(stats.GetLabels()[l])])
             csf_ids.sort(key = sortSecond, reverse = True)
-            subprocess.call("echo " + "SUCCEEDED AT ::{}  > error.txt".format(inspect.stack()[0][3]) ,shell=True )
+            # subprocess.call("echo " + "SUCCEEDED AT ::{}  > error.txt".format(inspect.stack()[0][3]) ,shell=True )
             first_seg_centroid=np.array(stats.GetCentroid(stats.GetLabels()[csf_ids[0][0]]))
             second_seg_centroid=np.array(stats.GetCentroid(stats.GetLabels()[csf_ids[1][0]]))
             bet_centroid=np.array(stats.GetCentroid(stats.GetLabels()[id_of_maxsize_comp_1]))
