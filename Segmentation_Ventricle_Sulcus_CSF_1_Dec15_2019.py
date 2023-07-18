3733bc9048161c4b3c775da5d800993102beda5a
dharlabwustl <atulkumar@wustl.edu>
2023-07-18 15:54:58 -0500

    First Commit

--- Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
+++ Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
@@ -175,7 +175,7 @@ def divideintozones_v1(filename_gray,filename_mask,filename_bet):
 
             initial_seed_point_indexes=[stats.GetMinimumIndex(stats.GetLabels()[id_of_maxsize_comp])]
             seg_explicit_thresholds = sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-            subprocess.call("echo " + "SUCCEED AT ::{}  > error.txt".format(inspect.stack()[0][3]) ,shell=True )
+            # subprocess.call("echo " + "SUCCEED AT ::{}  > error.txt".format(inspect.stack()[0][3]) ,shell=True )
     #         zoneV_min_z,zoneV_max_z=get_ventricles_range(sitk.GetArrayFromImage(seg_explicit_thresholds))
     #         subtracted_image=subtract_binary_1(sitk.GetArrayFromImage(img_T1_Copy),sitk.GetArrayFromImage(seg_explicit_thresholds)*255)
     #         subtracted_image=sitk.GetImageFromArray(subtracted_image)
