bd27733f09f25b57319412a0a91b1abbda3babb9
dharlabwustl <atulkumar@wustl.edu>
2023-07-27 15:51:26 -0500

    First Commit

--- compartment_separation.sh
+++ compartment_separation.sh
@@ -16,8 +16,8 @@ function call_get_resourcefiles_metadata_saveascsv_args() {
   #  local file_ext=${5}
   #  local output_csvfile=${output_csvfile%.*}${resource_dir}.csv
 
-  local working_dir=${3}
-  local call_download_files_in_a_resource_in_a_session_arguments=('call_get_resourcefiles_metadata_saveascsv_args' ${URI} ${resource_dir} ${working_dir} ${output_csvfile})
+  local final_output_directory=${3}
+  local call_download_files_in_a_resource_in_a_session_arguments=('call_get_resourcefiles_metadata_saveascsv_args' ${URI} ${resource_dir} ${final_output_directory} ${output_csvfile})
   outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")
   echo " I AM AT call_get_resourcefiles_metadata_saveascsv_args"
 
@@ -25,101 +25,95 @@ function call_get_resourcefiles_metadata_saveascsv_args() {
 echo " I AM RUNNING "
 ################ DOWNLOAD MASKS ###############################
 ## METADATA in the MASK directory
-URI=/data/experiments/${sessionID}/resources
+URI=/data/experiments/${sessionID}
 resource_dir="NIFTI_LOCATION"
 output_csvfile=${sessionID}_SCANSELECTION_METADATA.csv
-call_download_files_in_a_resource_in_a_session_arguments=('call_get_resourcefiles_metadata_saveascsv_args' ${URI} ${resource_dir} ${working_dir} ${output_csvfile})
-outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")
-#call_get_resourcefiles_metadata_saveascsv_args ${URI} ${resource_dir} ${working_dir} ${output_csvfile}
-echo output_csvfile::${working_dir}/${output_csvfile}::outputfiles_present::${outputfiles_present}
-#dir_to_save=${working_dir}
-#greyfile="NONE" ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset.nii.gz'
-#betfile="NONE"  ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset_bet.nii.gz'
-#csffile="NONE"  ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_final_seg.nii.gz'
-#while IFS=',' read -ra array; do
-#  #  #xx=0
-#  #  #
-#  #  ##if [ ${array[1]} == "SNIPR01_E00894" ]  ; then
-#  #  #  echo "${array[6]}"
-#  url=${array[6]}
-#  filename=$(basename ${url})
-#  echo ${filename}
-#  echo ${url}
-#  #
-#  #  #def call_download_a_singlefile_with_URIString(args):
-#  #  #    url=args.stuff[1]
-#  #  #    filename=args.stuff[2]
-#  #  #    dir_to_save=args.stuff[3]
-#  call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url} ${filename} ${dir_to_save})
-#  outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#  echo ${outputfiles_present}
-#  #
-##    while IFS=',' read -ra array1; do
-##  #    #      echo "${array1[0]}"
-##      url1=${array1[0]}
-##      echo url1::${url1}
-##  #    #      URI=/data/experiments/${sessionID}
-##      resource_dir="MASKS"
-##      output_csvfile_1=${sessionID}_SCANSELECTION_MASKS_METADATA.csv
-##      call_get_resourcefiles_metadata_saveascsv_args ${url1} ${resource_dir} ${working_dir} ${output_csvfile_1}
-#  #    #      filename1=$(basename ${url1})
-#  #    #  call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url1} ${filename1} ${dir_to_save})
-#  #    #  outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#  #
-##      while IFS=',' read -ra array2; do
-##  #
-##        url2=${array2[6]}
-##        echo url2::${url2}
-#  #      if [[ ${url2} == *"_levelset.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
-#  #        echo "It's there!"
-#  #        echo "${array2[6]}"
-#  #        filename2=$(basename ${url2})
-#  #        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
-#  #        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#  #        greyfile=${dir_to_save}/${filename2}
-#  #        echo "${greyfile}"
-#  #      fi
-#  #      if [[ ${url2} == *"_levelset_bet.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
-#  #        echo "It's there!"
-#  #        echo "${array2[6]}"
-#  #        filename2=$(basename ${url2})
-#  #        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
-#  #        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#  #        betfile=${dir_to_save}/${filename2}
-#  #        echo "${betfile}"
-#  #      fi
-#  #      if [[ ${url2} == *"_csf_unet.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
-#  #        echo "It's there!"
-#  #        echo "${array2[6]}"
-#  #        filename2=$(basename ${url2})
-#  #        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
-#  #        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#  #        csffile=${dir_to_save}/${filename2}
-#  #        echo "${csffile}"
-#  #      fi
-##      done < <(tail -n +2 "${working_dir}/${output_csvfile_1}")
-#  #
-##    done < <(tail -n +2 "${dir_to_save}/${filename}")
-#  #
-#done < <(tail -n +2 "${working_dir}/${output_csvfile}")
+call_get_resourcefiles_metadata_saveascsv_args ${URI} ${resource_dir} ${working_dir} ${output_csvfile}
+dir_to_save=${working_dir}
+greyfile="NONE" ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset.nii.gz'
+betfile="NONE"  ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset_bet.nii.gz'
+csffile="NONE"  ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_final_seg.nii.gz'
+while IFS=',' read -ra array; do
+  #xx=0
+  #
+  ##if [ ${array[1]} == "SNIPR01_E00894" ]  ; then
+  #  echo "${array[6]}"
+  url=${array[6]}
+  filename=$(basename ${url})
+
+  #def call_download_a_singlefile_with_URIString(args):
+  #    url=args.stuff[1]
+  #    filename=args.stuff[2]
+  #    dir_to_save=args.stuff[3]
+  call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url} ${filename} ${dir_to_save})
+  outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+
+  while IFS=',' read -ra array1; do
+    #      echo "${array1[0]}"
+    url1=${array1[0]}
+    #      URI=/data/experiments/${sessionID}
+    resource_dir="MASKS"
+    output_csvfile_1=${sessionID}_MASK_METADATA.csv
+    call_get_resourcefiles_metadata_saveascsv_args ${url1} ${resource_dir} ${working_dir} ${output_csvfile_1}
+    #      filename1=$(basename ${url1})
+    #  call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url1} ${filename1} ${dir_to_save})
+    #  outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+
+    while IFS=',' read -ra array2; do
+
+      url2=${array2[6]}
+      if [[ ${url2} == *"_levelset.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+        echo "It's there!"
+        echo "${array2[6]}"
+        filename2=$(basename ${url2})
+        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+        greyfile=${dir_to_save}/${filename2}
+        echo "${greyfile}"
+      fi
+      if [[ ${url2} == *"_levelset_bet.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+        echo "It's there!"
+        echo "${array2[6]}"
+        filename2=$(basename ${url2})
+        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+        betfile=${dir_to_save}/${filename2}
+        echo "${betfile}"
+      fi
+      if [[ ${url2} == *"_csf_unet.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+        echo "It's there!"
+        echo "${array2[6]}"
+        filename2=$(basename ${url2})
+        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+        csffile=${dir_to_save}/${filename2}
+        echo "${csffile}"
+      fi
+    done < <(tail -n +2 "${working_dir}/${output_csvfile_1}")
+
+  done \
+    < <(tail -n +2 "${dir_to_save}/${filename}")
+
+done \
+  < <(tail -n +2 "${working_dir}/${output_csvfile}")
+
+if [[ -f "${greyfile}" ]] && [[ -f "${betfile}" ]] && [[ -f "${csffile}" ]]; then
+  call_csf_compartments_arguments=('call_csf_compartments' ${greyfile} ${csffile} ${betfile})
+  outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
+  echo ${outputfiles_present}
+fi
+## single filename NECT, its CSF mask and other relevant files
+#rm /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/error.txt
+#greyfile='/workinginput/SAH_1_01052014_2003_2_resaved_levelset.nii.gz'
+### '/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset.nii.gz'
+#betfile='/workinginput/SAH_1_01052014_2003_2_resaved_levelset_bet.nii.gz'
+##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset_bet.nii.gz'
+#csffile='/workinginput/SAH_1_01052014_2003_2_resaved_csf_unet.nii.gz'
+##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_final_seg.nii.gz'
+#
+#call_csf_compartments_arguments=('call_csf_compartments' ${greyfile} ${csffile} ${betfile} )
+#outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}" )
+#echo ${outputfiles_present}
+###python /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/CSFCOMPARTMENT/CSF_COMPARTMENT_v1_part2_July18_2023.py
 ##
-##if [[ -f "${greyfile}" ]] && [[ -f "${betfile}" ]] && [[ -f "${csffile}" ]]; then
-##  call_csf_compartments_arguments=('call_csf_compartments' ${greyfile} ${csffile} ${betfile})
-##  outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
-##  echo ${outputfiles_present}
-##fi
-#### single filename NECT, its CSF mask and other relevant files
-###rm /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/error.txt
-###greyfile='/workinginput/SAH_1_01052014_2003_2_resaved_levelset.nii.gz'
-##### '/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset.nii.gz'
-###betfile='/workinginput/SAH_1_01052014_2003_2_resaved_levelset_bet.nii.gz'
-####'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset_bet.nii.gz'
-###csffile='/workinginput/SAH_1_01052014_2003_2_resaved_csf_unet.nii.gz'
-####'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_final_seg.nii.gz'
-###
-###call_csf_compartments_arguments=('call_csf_compartments' ${greyfile} ${csffile} ${betfile} )
-###outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}" )
-###echo ${outputfiles_present}
-#####python /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/CSFCOMPARTMENT/CSF_COMPARTMENT_v1_part2_July18_2023.py
-####
-#####pdflatex /media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/SAH_N_CSF_Compartment/RESULTS/test.tex
+###pdflatex /media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/SAH_N_CSF_Compartment/RESULTS/test.tex
