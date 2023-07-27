07d13f5ef3b48f7124c8f960f5b01bad53c1313f
dharlabwustl <atulkumar@wustl.edu>
2023-07-27 15:37:05 -0500

    First Commit

--- compartment_separation.sh
+++ compartment_separation.sh
@@ -28,7 +28,9 @@ echo " I AM RUNNING "
 URI=/data/experiments/${sessionID}/resources
 resource_dir="NIFTI_LOCATION"
 output_csvfile=${sessionID}_SCANSELECTION_METADATA.csv
-call_get_resourcefiles_metadata_saveascsv_args ${URI} ${resource_dir} ${working_dir} ${output_csvfile}
+call_download_files_in_a_resource_in_a_session_arguments=('call_get_resourcefiles_metadata_saveascsv_args' ${URI} ${resource_dir} ${working_dir} ${output_csvfile})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")
+#call_get_resourcefiles_metadata_saveascsv_args ${URI} ${resource_dir} ${working_dir} ${output_csvfile}
 echo output_csvfile::${working_dir}/${output_csvfile}
 #dir_to_save=${working_dir}
 #greyfile="NONE" ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset.nii.gz'
