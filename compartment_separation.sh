18c5613fefd1de25ddd153356bdfee5198f60572
dharlabwustl <atulkumar@wustl.edu>
2023-07-27 15:52:55 -0500

    First Commit

--- compartment_separation.sh
+++ compartment_separation.sh
@@ -99,7 +99,7 @@ done \
 if [[ -f "${greyfile}" ]] && [[ -f "${betfile}" ]] && [[ -f "${csffile}" ]]; then
   call_csf_compartments_arguments=('call_csf_compartments' ${greyfile} ${csffile} ${betfile})
   outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
-  echo ${outputfiles_present}
+#  echo ${outputfiles_present}
 fi
   echo ${outputfiles_present}
 ## single filename NECT, its CSF mask and other relevant files
