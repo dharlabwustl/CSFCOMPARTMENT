b3c8e3ab5e3cba7e492be172ae0f9fe3f99118a8
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-06-12 15:03:35 -0500

    fhgfhgfhg

--- CSF_COMPARTMENT_GITHUB_July212023.py
+++ CSF_COMPARTMENT_GITHUB_July212023.py
@@ -8,17 +8,40 @@ Created on Thu Sep  5 10:26:44 2019
 """
 
 import sys,argparse,inspect
-
-
-
 from  Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019 import * 
+def csf_compartments_ventbound_given(filename_gray,filename_mask,filename_bet,zoneV_min_z,zoneV_max_z):
+    returnvalue=0
+    try:
 
+        sulci_vol, ventricle_vol,leftcountven,rightcountven,leftcountsul,rightcountsul,sulci_vol_above_vent,sulci_vol_below_vent,sulci_vol_at_vent = divideintozones_v1(filename_gray,filename_mask,filename_bet)
+
+        print("I SUCCEED AT ::{}".format(inspect.stack()[0][3]))
+        returnvalue=1
+    except:
+        print("I FAILED AT ::{}".format(inspect.stack()[0][3]))
+        pass
+    return returnvalue
 def csf_compartments(filename_gray,filename_mask,filename_bet):
     returnvalue=0
     try:
 
         sulci_vol, ventricle_vol,leftcountven,rightcountven,leftcountsul,rightcountsul,sulci_vol_above_vent,sulci_vol_below_vent,sulci_vol_at_vent = divideintozones_v1(filename_gray,filename_mask,filename_bet)
 
+        print("I SUCCEED AT ::{}".format(inspect.stack()[0][3]))
+        returnvalue=1
+    except:
+        print("I FAILED AT ::{}".format(inspect.stack()[0][3]))
+        pass
+    return returnvalue
+def call_csf_compartments_ventbound_given(args):
+    returnvalue=0
+    try:
+        filename_gray=args.stuff[1]
+        filename_mask=args.stuff[2]
+        filename_bet=args.stuff[3]
+        zoneV_min_z=args.stuff[4]
+        zoneV_max_z=args.stuff[5]
+        csf_compartments_ventbound_given(filename_gray,filename_mask,filename_bet,zoneV_min_z,zoneV_max_z)
         print("I SUCCEED AT ::{}".format(inspect.stack()[0][3]))
         returnvalue=1
     except:
@@ -48,8 +71,13 @@ def main():
     name_of_the_function=args.stuff[0]
     return_value=0
     if name_of_the_function == "call_csf_compartments":
-        return_value=call_csf_compartments(args)
+        return_value=call_csf_compartments(args) #
+
+    if name_of_the_function == "call_csf_compartments_ventbound_given":
+        return_value=call_csf_compartments_ventbound_given(args) 
+
     return return_value
+
 
 if __name__ == '__main__':
     main()
