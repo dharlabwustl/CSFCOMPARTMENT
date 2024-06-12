4acafe2e76a4003fb3d73dcab54fd1890ffe49db
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-06-12 16:00:08 -0500

    asdhkfjhkdjf

--- project_level_csf_compartment.sh
+++ project_level_csf_compartment.sh
@@ -52,7 +52,7 @@ count=0
 
     # echo SESSION_NAME::${SESSION_NAME}
     directory_to_create_destroy
-    /software/compartment_separation.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
+    /software/compartment_separation_with_vent_boundgiven.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
     # echo snipr_step::${snipr_step}
     # scan_selection ${SESSION_ID}  
 
