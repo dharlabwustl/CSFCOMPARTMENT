3efb0ce7fcb5b71a163c395cc10ae4c039d998a0
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-06-13 13:02:05 -0500

    cdfgcdgf

--- project_level_csf_compartment.sh
+++ project_level_csf_compartment.sh
@@ -87,7 +87,7 @@ URI_COL_NUM=$(get_column_number ${working_dir}/$output_csvfile URI)
 echo ${URI_COL_NUM}
   while IFS=',' read -ra array0; do
   echo ${array0[$((URI_COL_NUM=))]}
-done < <(tail -n +2 "${sessions_list}")
+done < <(tail -n +2 "${output_csvfile}")
 #
 # ##############################
 # # # Get the header row and split it into columns
