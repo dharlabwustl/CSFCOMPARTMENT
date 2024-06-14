6af817d6d87e71cadf1deb94a135017a9c7c78ba
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-06-14 00:44:31 -0500

    adfdsf

--- compartment_separation_with_vent_boundgiven_specific_nifit.sh
+++ compartment_separation_with_vent_boundgiven_specific_nifit.sh
@@ -51,9 +51,10 @@ while IFS=',' read -ra array; do
   #    url=args.stuff[1]
   #    filename=args.stuff[2]
   #    dir_to_save=args.stuff[3]
+    echo required_nifti::${required_nifti}::${url}
     if [[ ${url} != *"$required_nifti"* ]]; then #echo "$url2" | grep -q "$required_nifti"; then 
-
-    break
+    
+    continue
     fi
   call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url} ${filename} ${dir_to_save})
   outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
