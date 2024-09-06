d71f47d5ef05e8dbcd341566cd3d8d1fc6c2a37e
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-09-06 18:10:04 -0400

    sdfdfdf

--- CSF_COMPARTMENT_GITHUB_July212023.py
+++ CSF_COMPARTMENT_GITHUB_July212023.py
@@ -71,7 +71,7 @@ def main():
         parser.add_argument('stuff', nargs='+')
         args = parser.parse_args()
         name_of_the_function=args.stuff[0]
-        print(name_of_the_function)
+        print('name of the function is ' + name_of_the_function)
         print("WO ZAI ::{}".format("main"))
         if name_of_the_function == "call_csf_compartments":
             print("WO ZAI ::{}".format("call_csf_compartments"))
