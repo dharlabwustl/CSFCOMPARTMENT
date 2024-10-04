9e53b6d42cd6ae258ba64e242171ea3fd3ad22f3
dharlabwustl <atulkumar@wustl.edu>
2024-10-04 15:11:21 -0500

    First Commit

--- compartment_separation_with_vent_boundgiven_insnipr.sh
+++ compartment_separation_with_vent_boundgiven_insnipr.sh
@@ -116,7 +116,7 @@ while IFS=',' read -ra array; do
     ##############################################
         resource_dir="PREPROCESS_SEGM"
         output_csvfile_2=${sessionID}_PREPROCESS_SEGM_METADATA.csv
-        call_get_resourcefiles_metadata_saveascsv_args ${url1} ${resource_dir} ${working_dir} ${output_csvfile_1}
+        call_get_resourcefiles_metadata_saveascsv_args ${url1} ${resource_dir} ${working_dir} ${output_csvfile_2}
         #      filename1=$(basename ${url1})
         #  call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url1} ${filename1} ${dir_to_save})
         #  outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
