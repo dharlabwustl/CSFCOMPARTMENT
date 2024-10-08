a1313543069377f20e3512a837af61beda17ccd2
dharlabwustl <atulkumar@wustl.edu>
2024-10-08 16:27:20 -0500

    subsequent  Commits

--- compartment_separation_with_vent_boundgiven_insnipr.sh
+++ compartment_separation_with_vent_boundgiven_insnipr.sh
@@ -168,7 +168,7 @@ while IFS=',' read -ra array; do
       echo ${file_name}
       if [[ ${file_name} == *"ventricle"* ]] || [[ ${file_name} == *"sulci"* ]]; then
         call_uploadsinglefile_with_URI_arguments=('call_uploadsinglefile_with_URI' ${URI_1} ${file_name} ${resource_dirname})
-#        outputfiles_present=$(python3 /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
+        outputfiles_present=$(python3 /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
         echo ${outputfiles_present}
 
       fi
