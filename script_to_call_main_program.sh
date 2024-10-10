cf735f3e3749007ca1b88b9b8e99c4f07caff9cc
dharlabwustl <atulkumar@wustl.edu>
2024-10-10 11:35:51 -0500

    subsequent  Commadfits

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -38,7 +38,7 @@ fi
 if [[ ${TYPE_OF_PROGRAM} == "VENT_BOUND_IN_SNIPR_DEBUG" ]] ;
 then
 echo " I AM IN TYPE_OF_PROGRAM == VENT_BOUND_IN_SNIPR_DEBUG"
-/software/compartment_separation_with_vent_boundgiven_insnipr.sh  $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST
+/software/compartment_separation_with_vent_afterdeepreg.sh  $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST
 fi
 if [[ ${TYPE_OF_PROGRAM} == 1 ]] ;
 then
