44442421e9942a87fe1f16947c1c6060e55c7054
dharlabwustl <atulkumar@wustl.edu>
2024-10-12 15:49:48 -0500

    subsequent  Commadfits

--- Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
+++ Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
@@ -461,7 +461,7 @@ def divideintozones_with_vent_obb(filename_gray,filename_mask,filename_bet,filen
                     id_of_maxsize_comp=csf_ids[0][0]
 
             initial_seed_point_indexes=[stats.GetMinimumIndex(stats.GetLabels()[id_of_maxsize_comp])]
-            seg_explicit_thresholds =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
+            seg_explicit_thresholds =img_T1 ##sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
 
             zoneV_min_z,zoneV_max_z=get_ventricles_range(sitk.GetArrayFromImage(seg_explicit_thresholds))
             subtracted_image=subtract_binary_1(sitk.GetArrayFromImage(img_T1_1),sitk.GetArrayFromImage(seg_explicit_thresholds)*255)
