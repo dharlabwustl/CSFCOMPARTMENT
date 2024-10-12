62824e4e93739710cbc8e08a53526843468df9b6
dharlabwustl <atulkumar@wustl.edu>
2024-10-11 19:31:58 -0500

    subsequent  Commadfits

--- compartment_separation_with_vent_afterdeepreg.sh
+++ compartment_separation_with_vent_afterdeepreg.sh
@@ -166,15 +166,15 @@ echo "csffile:::::ATUL:::${csffile}"
     filename_prefix=$(basename ${url}) #${url2%/resource*} #filename=
     filename_prefix=${filename_prefix%_NIFTILOCATION*}
     resource_dirname="MASKS"
-#    for file_name in ${dir_to_save}/${filename_prefix}*.nii.gz; do
-#      echo ${file_name}
-#      if [[ ${file_name} == *"ventricle"* ]] || [[ ${file_name} == *"sulci"* ]]; then
-#        call_uploadsinglefile_with_URI_arguments=('call_uploadsinglefile_with_URI' ${URI_1} ${file_name} ${resource_dirname})
-#        outputfiles_present=$(python3 /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
-#        echo ${outputfiles_present}
-#
-#      fi
-#    done
+    for file_name in ${dir_to_save}/${filename_prefix}*.nii.gz; do
+      echo ${file_name}
+      if [[ ${file_name} == *"ventricle"* ]] || [[ ${file_name} == *"sulci"* ]]; then
+        call_uploadsinglefile_with_URI_arguments=('call_uploadsinglefile_with_URI' ${URI_1} ${file_name} ${resource_dirname})
+        outputfiles_present=$(python3 /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
+        echo ${outputfiles_present}
+
+      fi
+    done
   done < <(tail -n +2 "${dir_to_save}/${filename}")
 
 done \
