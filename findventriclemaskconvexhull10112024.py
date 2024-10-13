bd33ce35d940cb29f0637a2a81f94d8236a81e52
dharlabwustl <atulkumar@wustl.edu>
2024-10-12 22:33:07 -0500

    subsequent  Commadfits

--- findventriclemaskconvexhull10112024.py
+++ findventriclemaskconvexhull10112024.py
@@ -55,159 +55,6 @@ import os
 import nibabel as nib
 import numpy as np
 import glob
-def compute_obb(binary_mask):
-    """
-    Compute the Oriented Bounding Box (OBB) for the non-zero elements in a 3D binary mask using PCA.
-
-    Parameters:
-    - binary_mask: 3D numpy array (binary mask of the object)
-
-    Returns:
-    - obb_corners: The 8 corner points of the OBB (in the original space)
-    - pca_components: The principal components (directions of the OBB)
-    - obb_center: The center point of the OBB (in the original space)
-    - radii: The half-lengths of the OBB along the principal axes
-    """
-    # Find non-zero points (the object)
-    points = np.column_stack(np.nonzero(binary_mask))
-
-    if points.size == 0:
-        # If no object, return empty results
-        return None, None, None, None
-
-    # Apply PCA to get the principal axes of the object
-    pca = PCA(n_components=3)
-    pca.fit(points)
-
-    # Get the OBB center and its half-lengths (radii)
-    obb_center = np.mean(points, axis=0)
-    points_transformed = pca.transform(points)
-    radii = (np.max(points_transformed, axis=0) - np.min(points_transformed, axis=0)) / 2
-
-    # Get the 8 corners of the OBB
-    obb_corners_local = np.array(list(product([-1, 1], repeat=3))) * radii
-    obb_corners = pca.inverse_transform(obb_corners_local + pca.transform([obb_center])[0])
-
-    return obb_corners, pca.components_, obb_center, radii
-def proportional_subdivide_obb(obb_center, radii, pca_components, n_subdivisions):
-    """
-    Subdivide the Oriented Bounding Box (OBB) proportionally to its radii (dimensions) along the
-    principal axes.
-
-    Parameters:
-    - obb_center: The center of the OBB (in the original space)
-    - radii: The half-lengths of the OBB along each principal axis
-    - pca_components: The principal components (rotation matrix of the OBB)
-    - n_subdivisions: Number of divisions along the longest axis; the other axes will be divided
-      proportionally.
-
-    Returns:
-    - centroids: A list of centroids of each sub-box (in the original space)
-    """
-    # Step 1: Proportionally subdivide based on the radii along each axis
-    longest_axis_idx = np.argmax(radii)
-    longest_axis_length = radii[longest_axis_idx]
-
-    # Calculate the number of subdivisions along each axis based on the proportion to the longest axis
-    subdivisions_per_axis = np.ceil(n_subdivisions * (radii / longest_axis_length)).astype(int)
-
-    # Ensure that we at least have one subdivision along each axis
-    subdivisions_per_axis[subdivisions_per_axis == 0] = 1
-
-    # Step 2: Generate the ranges for the subdivisions along each axis in the local OBB space
-    ranges = [np.linspace(-radii[i], radii[i], subdivisions_per_axis[i] + 1) for i in range(3)]
-
-    centroids = []
-
-    for i, j, k in product(range(subdivisions_per_axis[0]), range(subdivisions_per_axis[1]), range(subdivisions_per_axis[2])):
-        # Min and max corners in local OBB space
-        sub_box_min_local = np.array([ranges[0][i], ranges[1][j], ranges[2][k]])
-        sub_box_max_local = np.array([ranges[0][i+1], ranges[1][j+1], ranges[2][k+1]])
-
-        # Compute the centroid in local space
-        centroid_local = (sub_box_min_local + sub_box_max_local) / 2
-
-        # Convert centroid to original space using the OBB's rotation and translation
-        centroid_original = pca_components.T @ centroid_local + obb_center
-        centroids.append(centroid_original)
-
-    return centroids
-def find_closest_non_zero_voxel(binary_mask, centroids):
-    """
-    For each centroid, find the closest non-zero voxel in the binary mask.
-
-    Parameters:
-    - binary_mask: 3D numpy array (binary mask of the object)
-    - centroids: List of centroids from the subdivided boxes
-
-    Returns:
-    - closest_voxels: List of coordinates of the closest non-zero voxels to the centroids
-    """
-    non_zero_voxels = np.column_stack(np.nonzero(binary_mask))
-
-    if non_zero_voxels.size == 0:
-        return []
-
-    closest_voxels = []
-
-    for centroid in centroids:
-        distances = distance.cdist([centroid], non_zero_voxels)
-        closest_idx = np.argmin(distances)
-        closest_voxels.append(non_zero_voxels[closest_idx])
-
-    return closest_voxels
-def create_obb_binary_mask(binary_mask, obb_corners):
-    """
-    Create a binary mask representing the OBB by filling in the OBB region.
-
-    Parameters:
-    - binary_mask: 3D numpy array (binary mask of the object)
-    - obb_corners: The 8 corner points of the OBB (in the original space)
-
-    Returns:
-    - obb_mask: 3D numpy array with the OBB region filled with 1s
-    """
-    # Create an empty mask of the same shape as the original binary mask
-    obb_mask = np.zeros_like(binary_mask)
-
-    # Get the bounding box of the OBB (min and max coordinates along each axis)
-    x_min, y_min, z_min = np.floor(np.min(obb_corners, axis=0)).astype(int)
-    x_max, y_max, z_max = np.ceil(np.max(obb_corners, axis=0)).astype(int)
-
-    # Fill the region inside the OBB (axis-aligned for simplicity in this example)
-    obb_mask[x_min:x_max+1, y_min:y_max+1, z_min:z_max+1] = 1
-
-    return obb_mask
-
-def process_binary_mask_with_obb(binary_mask, n_subdivisions=8):
-    """
-    Process the binary mask to compute the OBB, subdivide it, and find closest non-zero voxels.
-    Also returns the OBB binary mask.
-
-    Parameters:
-    - binary_mask: 3D numpy array (binary mask of the object)
-    - n_subdivisions: Number of equal-sized sub-boxes to divide the OBB into
-
-    Returns:
-    - closest_voxels: Coordinates of the closest non-zero voxels to the centroids
-    - obb_mask: Binary mask of the OBB
-    """
-    # Step 1: Compute the OBB (Oriented Bounding Box)
-    obb_corners, pca_components, obb_center, radii = compute_obb(binary_mask)
-
-    if obb_corners is None:
-        return None, None
-
-    # Step 2: Subdivide the OBB and find the centroids of the subdivided boxes
-    centroids = proportional_subdivide_obb(obb_center, radii, pca_components, n_subdivisions)
-
-    # Step 3: Find the closest non-zero voxels to the centroids in the original binary mask
-    closest_voxels = find_closest_non_zero_voxel(binary_mask, centroids)
-
-    # Step 4: Create the binary mask of the OBB
-    obb_mask = create_obb_binary_mask(binary_mask, obb_corners)
-
-    return closest_voxels, obb_mask
 
 def sortSecond(val):
     return val[1]
@@ -524,7 +371,7 @@ def divideintozones_v1_with_vent_bound(filename_gray,filename_mask,filename_bet,
     return  sulci_vol, ventricle_vol,leftcountven,rightcountven,leftcountsul,rightcountsul,sulci_vol_above_vent,sulci_vol_below_vent,sulci_vol_at_vent
     # return sulci_vol, ventricle_vol,leftcountven*resol,rightcountven*resol,leftcountsul*resol,rightcountsul*resol,sulci_vol_above_vent,sulci_vol_below_vent,sulci_vol_at_vent #seg_explicit_thresholds, subtracted_image
 
-def divideintozones_with_vent_convexhull(filename_gray,filename_mask,filename_bet,filename_vent_conv_hull,closest_voxels):
+def divideintozones_with_vent_convexhull(filename_gray,filename_mask,filename_bet,filename_vent_conv_hull):
     try:
         sulci_vol, ventricle_vol,leftcountven,rightcountven,leftcountsul,rightcountsul,sulci_vol_above_vent,sulci_vol_below_vent,sulci_vol_at_vent=(0,0,0,0,0,0,0,0,0) #seg_explicit_thresholds, subtracted_image
         #######################
@@ -635,40 +482,8 @@ def divideintozones_with_vent_convexhull(filename_gray,filename_mask,filename_be
                 else:
                     id_of_maxsize_comp=csf_ids[0][0]
 
-            # initial_seed_point_indexes=[stats.GetMinimumIndex(stats.GetLabels()[id_of_maxsize_comp])]
-            # seg_explicit_thresholds=img_T1 ##sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
             initial_seed_point_indexes=[stats.GetMinimumIndex(stats.GetLabels()[id_of_maxsize_comp])]
-            print('initial_seed_point_indexes::{}'.format(type(initial_seed_point_indexes[0])))
-            initial_seed_point_indexes=[(int(closest_voxels[0][0]),int(closest_voxels[0][1]),int(closest_voxels[0][2]))]
-            print('initial_seed_point_indexes_nw::{}'.format(type(initial_seed_point_indexes[0])))
-            seg_explicit_thresholds =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-            initial_seed_point_indexes=[(int(closest_voxels[1][0]),int(closest_voxels[1][1]),int(closest_voxels[1][2]))]
-            seg_explicit_thresholds1 =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-            initial_seed_point_indexes=[(int(closest_voxels[2][0]),int(closest_voxels[2][1]),int(closest_voxels[2][2]))]
-            seg_explicit_thresholds2 =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-            initial_seed_point_indexes=[(int(closest_voxels[3][0]),int(closest_voxels[3][1]),int(closest_voxels[3][2]))]
-            seg_explicit_thresholds3 =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-            initial_seed_point_indexes=[(int(closest_voxels[4][0]),int(closest_voxels[4][1]),int(closest_voxels[4][2]))]
-            seg_explicit_thresholds4 =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-            initial_seed_point_indexes=[(int(closest_voxels[5][0]),int(closest_voxels[5][1]),int(closest_voxels[5][2]))]
-            seg_explicit_thresholds5 =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-            initial_seed_point_indexes=[(int(closest_voxels[6][0]),int(closest_voxels[6][1]),int(closest_voxels[6][2]))]
-            seg_explicit_thresholds6 =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-            initial_seed_point_indexes=[(int(closest_voxels[7][0]),int(closest_voxels[7][1]),int(closest_voxels[7][2]))]
-            seg_explicit_thresholds7 =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-            initial_seed_point_indexes=[(int(closest_voxels[8][0]),int(closest_voxels[8][1]),int(closest_voxels[8][2]))]
-            seg_explicit_thresholds8 =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
-
-
-            seg_explicit_thresholds = sitk.Or(seg_explicit_thresholds > 0, seg_explicit_thresholds1 > 0)
-            seg_explicit_thresholds = sitk.Or(seg_explicit_thresholds, seg_explicit_thresholds2 > 0)
-            seg_explicit_thresholds = sitk.Or(seg_explicit_thresholds, seg_explicit_thresholds3 > 0)
-            seg_explicit_thresholds = sitk.Or(seg_explicit_thresholds, seg_explicit_thresholds4 > 0)
-            seg_explicit_thresholds = sitk.Or(seg_explicit_thresholds, seg_explicit_thresholds5 > 0)
-            seg_explicit_thresholds = sitk.Or(seg_explicit_thresholds, seg_explicit_thresholds5 > 0)
-            seg_explicit_thresholds = sitk.Or(seg_explicit_thresholds, seg_explicit_thresholds6 > 0)
-            seg_explicit_thresholds = sitk.Or(seg_explicit_thresholds, seg_explicit_thresholds7 > 0)
-            seg_explicit_thresholds = sitk.Or(seg_explicit_thresholds, seg_explicit_thresholds8 > 0)
+            seg_explicit_thresholds =img_T1 ##sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
 
             zoneV_min_z,zoneV_max_z=get_ventricles_range(sitk.GetArrayFromImage(seg_explicit_thresholds))
             subtracted_image=subtract_binary_1(sitk.GetArrayFromImage(img_T1_1),sitk.GetArrayFromImage(seg_explicit_thresholds)*255)
@@ -819,32 +634,6 @@ def stl_to_binary_mask(stl_filename,inputniftifilename, output_nifti_filename, v
     nib.save(nifti_img, output_nifti_filename)
 
     print(f"Binary mask saved as {output_nifti_filename}")
-def find_closest_non_zero_voxel(binary_mask, centroids):
-    """
-    For each centroid, find the closest non-zero voxel in the binary mask.
-
-    Parameters:
-    - binary_mask: 3D numpy array (binary mask of the object)
-    - centroids: List of centroids from the subdivided boxes
-
-    Returns:
-    - closest_voxels: List of coordinates of the closest non-zero voxels to the centroids
-    """
-    non_zero_voxels = np.column_stack(np.nonzero(binary_mask))
-
-    if non_zero_voxels.size == 0:
-        return []
-
-    closest_voxels = []
-
-    for centroid in centroids:
-        distances = distance.cdist([centroid], non_zero_voxels)
-        closest_idx = np.argmin(distances)
-        closest_voxels.append(non_zero_voxels[closest_idx])
-
-    return closest_voxels
-
-
 def binarymask_to_convexhull_mask(input_nii_path,output_nii_path):
     # Step 1: Load the input binary mask from a NIfTI file
     # input_nii_path = 'workinginput/ventricle.nii'
@@ -870,7 +659,6 @@ def binarymask_to_convexhull_mask(input_nii_path,output_nii_path):
     convex_hull_mesh = trimesh.load('convex_hull_stl_1.stl') #'convex_hull_fixed_normals.stl')
     stl_to_binary_mask('convex_hull_stl_1.stl',input_nii_path, output_nii_path, binary_mask.shape)
 ventricle_mask=resizeinto_512by512_and_flip(nib.load(sys.argv[1]).get_fdata())
-centroids, ventricle_obb_mask = process_binary_mask_with_obb(ventricle_mask,n_subdivisions=9)
 csf_mask_nib=nib.load(sys.argv[2])
 array_img = nib.Nifti1Image(ventricle_mask, affine=csf_mask_nib.affine, header=csf_mask_nib.header)
 nib.save(array_img, os.path.join(sys.argv[3],'ventricle.nii'))
@@ -881,5 +669,4 @@ filename_mask=sys.argv[2]
 filename_bet=sys.argv[5]
 filename_vent_conv_hull=output_nii_path #os.path.join(sys.argv[3],'ventricle_convexhull_mask.nii')
 binarymask_to_convexhull_mask(input_nii_path,output_nii_path)
-closest_voxels=find_closest_non_zero_voxel(nib.load(filename_mask).get_fdata(), centroids)
-divideintozones_with_vent_convexhull(filename_gray,filename_mask,filename_bet,filename_vent_conv_hull,closest_voxels)
+divideintozones_with_vent_convexhull(filename_gray,filename_mask,filename_bet,filename_vent_conv_hull)
