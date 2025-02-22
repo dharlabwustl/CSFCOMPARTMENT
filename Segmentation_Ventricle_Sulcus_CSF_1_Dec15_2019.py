b13cacf2122e03515a3a32a41328e8a9879ef348
dharlabwustl <atulkumar@wustl.edu>
2025-02-21 21:27:08 -0600

    subsequent  Commadfits

--- Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
+++ Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
@@ -652,7 +652,8 @@ def divideintozones_with_vent_obb_ven_hem_given(filename_gray,filename_mask,file
             seg_explicit_thresholds =sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
             img_T1_temp_np_1=sitk.GetArrayFromImage(seg_explicit_thresholds)
             img_T1_temp_np_1[vent_hem_mask_itk_object_np>0]=1.0
-            zoneV_min_z,zoneV_max_z=get_ventricles_range(img_T1_temp_np_1) #sitk.GetArrayFromImage(seg_explicit_thresholds))
+            seg_explicit_thresholds=sitk.GetImageFromArray(img_T1_temp_np_1)
+            zoneV_min_z,zoneV_max_z=get_ventricles_range(sitk.GetArrayFromImage(seg_explicit_thresholds))
             subtracted_image=subtract_binary_1(sitk.GetArrayFromImage(img_T1_1),sitk.GetArrayFromImage(seg_explicit_thresholds)*255)
             subtracted_image=sitk.GetImageFromArray(subtracted_image)
             above_ventricle_image= sitk.GetArrayFromImage(subtracted_image)
