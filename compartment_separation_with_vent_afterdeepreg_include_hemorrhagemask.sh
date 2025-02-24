80f8ad499353fa98ca866ee94eb590c08c33f2d2
dharlabwustl <atulkumar@wustl.edu>
2025-02-24 13:34:41 -0600

    subsequent  Commadfits

--- compartment_separation_with_vent_afterdeepreg_include_hemorrhagemask.sh
+++ compartment_separation_with_vent_afterdeepreg_include_hemorrhagemask.sh
@@ -211,15 +211,14 @@ echo "csffile:::::ATUL:::${csffile}"
     filename_prefix=$(basename ${url}) #${url2%/resource*} #filename=
     filename_prefix=${filename_prefix%_NIFTILOCATION*}
     resource_dirname="MASKS"
-          this_data_basename=$(basename {greyfile})
-          this_data_basename_noext=${this_data_basename%_resaved*}
+    this_data_basename=$(basename ${greyfile})
+    this_data_basename_noext=${this_data_basename%_resaved*}
     for file_name in ${dir_to_save}/${filename_prefix}*.nii.gz; do
       echo ${file_name}
       if [[ ${file_name} == *"${this_data_basename_noext}"* ]] || [[ ${file_name} == *"ventricle"* ]] || [[ ${file_name} == *"sulci"* ]]; then
         call_uploadsinglefile_with_URI_arguments=('call_uploadsinglefile_with_URI' ${URI_1} ${file_name} ${resource_dirname})
         outputfiles_present=$(python3 /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
         echo ${outputfiles_present}
-
       fi
     done
     file_name=$(ls /input/${this_data_basename_noext}*_resaved_csf_unet_with_sah.nii.gz)
