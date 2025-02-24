59060b353f24c6516af7649185de04289d669afc
dharlabwustl <atulkumar@wustl.edu>
2025-02-24 13:37:59 -0600

    subsequent  Commadfits

--- compartment_separation_with_vent_afterdeepreg_include_hemorrhagemask.sh
+++ compartment_separation_with_vent_afterdeepreg_include_hemorrhagemask.sh
@@ -222,8 +222,9 @@ echo "csffile:::::ATUL:::${csffile}"
       fi
     done
     file_name=$(ls /input/${this_data_basename_noext}*_resaved_csf_unet_with_sah.nii.gz)
+    echo " ${URI_1} ${file_name} ${resource_dirname})"
     call_uploadsinglefile_with_URI_arguments=('call_uploadsinglefile_with_URI' ${URI_1} ${file_name} ${resource_dirname})
-    outputfiles_present=$(python3 /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
+#    outputfiles_present=$(python3 /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
     echo ${outputfiles_present}
   done < <(tail -n +2 "${dir_to_save}/${filename}")
 
