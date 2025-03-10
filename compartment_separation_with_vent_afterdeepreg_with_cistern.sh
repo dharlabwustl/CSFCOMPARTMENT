8969eb74243cb0f81ad21237a22f5cc0bab61a90
dharlabwustl <atulkumar@wustl.edu>
2025-03-10 13:25:59 -0500

    subsequent  Command fits

--- compartment_separation_with_vent_afterdeepreg_with_cistern.sh
+++ compartment_separation_with_vent_afterdeepreg_with_cistern.sh
@@ -184,7 +184,7 @@ echo "csffile:::::ATUL:::${csffile}"
 
     ################
 
-    echo "    call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )"
+    echo "call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )"
     call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )
     outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
     #  echo ${outputfiles_present}
