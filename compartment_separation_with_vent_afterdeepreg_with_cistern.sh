0c1ccae812ccd759253ce690507f256cb4fcf78f
dharlabwustl <atulkumar@wustl.edu>
2025-03-10 17:28:16 -0500

    subsequent  Command fits

--- compartment_separation_with_vent_afterdeepreg_with_cistern.sh
+++ compartment_separation_with_vent_afterdeepreg_with_cistern.sh
@@ -206,9 +206,9 @@ outputfiles_present=$(python3 download_with_session_ID.py "${function_with_argum
 
 ################
 
-echo "call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )"
-call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )
-outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
+#echo "call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )"
+#call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )
+#outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
 ##  echo ${outputfiles_present}
 #fi
 echo "call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem_with_cis' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )"
