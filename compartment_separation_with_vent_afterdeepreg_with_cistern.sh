f0fb9a9aaa9929b074805c9cb9a588c708deece8
dharlabwustl <atulkumar@wustl.edu>
2025-03-10 14:39:42 -0500

    subsequent  Command fits

--- compartment_separation_with_vent_afterdeepreg_with_cistern.sh
+++ compartment_separation_with_vent_afterdeepreg_with_cistern.sh
@@ -156,8 +156,8 @@ echo "${array2[6]}"
 filename2=$(basename ${url2})
 call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
 outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-cintern_only_mask=${dir_to_save}/${filename2}
-echo "${cintern_only_mask}"
+cistern_only_mask=${dir_to_save}/${filename2}
+echo "${cistern_only_mask}"
 fi
 
 if [[ ${url2} == *"warped_1_mov_midlinecssfResampled1"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
