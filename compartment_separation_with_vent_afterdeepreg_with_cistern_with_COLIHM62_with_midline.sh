e12423ba99f0153ac58d7f4214c71af789c16f1b
dharlabwustl <atulkumar@wustl.edu>
2025-04-18 15:20:46 -0500

    subsequent  Command fits

--- compartment_separation_with_vent_afterdeepreg_with_cistern_with_COLIHM62_with_midline.sh
+++ compartment_separation_with_vent_afterdeepreg_with_cistern_with_COLIHM62_with_midline.sh
@@ -70,126 +70,212 @@ done < <(tail -n +2 "${working_dir}/${output_csvfile}")
 #----------------------------------------
 get_scanID_from_sessionID ${sessionID} ${working_dir}
 echo ${sessionID}::${scanID}
-#----------------------------------------
-# Get scanID before main loop
-#----------------------------------------
+dir_to_save=${working_dir}
+greyfile="NONE" ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset.nii.gz'
+betfile="NONE"  ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_levelset_bet.nii.gz'
+csffile="NONE"  ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_final_seg.nii.gz'
+while IFS=',' read -ra array; do
+#xx=0
+#
+##if [ ${array[1]} == "SNIPR01_E00894" ]  ; then
+#  echo "${array[6]}"
+url=${array[6]}
+filename=$(basename ${url})
 
-#----------------------------------------
-# Download NIFTI_LOCATION Metadata
-#----------------------------------------
-#scanID=$(get_scanID_from_sessionID ${sessionID} ${working_dir})
-#echo ${scanID}
-#URI="/data/experiments/${sessionID}"
-#resource_dir="NIFTI_LOCATION"
-#output_csvfile="${sessionID}_SCANSELECTION_METADATA.csv"
-#call_get_resourcefiles_metadata_saveascsv_args ${URI} ${resource_dir} ${working_dir} ${output_csvfile}
-#
-#dir_to_save=${working_dir}
-#greyfile="NONE"
-#betfile="NONE"
-#csffile="NONE"
-#
-##----------------------------------------
-## Main Loop to Download and Process Files
-##----------------------------------------
-#while IFS=',' read -ra array; do
-#  url=${array[6]}
-#  filename=$(basename ${url})
-#
-#  call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url} ${filename} ${dir_to_save})
+#def call_download_a_singlefile_with_URIString(args):
+#    url=args.stuff[1]
+#    filename=args.stuff[2]
+#    dir_to_save=args.stuff[3]
+call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url} ${filename} ${dir_to_save})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+
+#call_download_files_in_a_resource_in_a_session_arguments=('call_download_files_in_a_resource_in_a_session' ${sessionID} "NIFTI_LOCATION" ${working_dir})
+#outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")
+
+while IFS=',' read -ra array1; do
+#      echo "${array1[0]}"
+url1=${array1[0]}
+#      URI=/data/experiments/${sessionID}
+resource_dir="MASKS"
+
+output_csvfile_1=${sessionID}_MASK_METADATA.csv
+call_get_resourcefiles_metadata_saveascsv_args ${url1} ${resource_dir} ${working_dir} ${output_csvfile_1}
+
+##    for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
+#niftifile_csvfilename=$(ls ${working_dir}/*NIFTILOCATION.csv)
+#while IFS=',' read -ra array5; do
+#scanID=${array5[2]}
+#echo sessionId::${sessionID}
+#echo scanId::${scanID}
+#done < <(tail -n +2 "${niftifile_csvfilename}")
+#      filename1=$(basename ${url1})
+#  call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url1} ${filename1} ${dir_to_save})
 #  outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} MASKS '_ventricle' ) ##'warped_1_mov_mri_region_' )
+#    echo "outputfiles_present="'$(python3 utilities_simple_trimmed.py' "${function_with_arguments[@]}"
+outputfiles_present=$(python3 download_with_session_ID.py "${function_with_arguments[@]}")
+function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} MASKS '_total' ) ##'warped_1_mov_mri_region_' )
+#    echo "outputfiles_present="'$(python3 utilities_simple_trimmed.py' "${function_with_arguments[@]}"
+outputfiles_present=$(python3 download_with_session_ID.py "${function_with_arguments[@]}")
+while IFS=',' read -ra array2; do
+
+url2=${array2[6]}
+
+if [[ ${url2} == *"_vertical_bounding_box_512x512.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+echo "It's there!"
+echo "${array2[6]}"
+filename2=$(basename ${url2})
+call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+greyfile=${dir_to_save}/${filename2}
+echo "${greyfile}"
+fi
+if [[ ${url2} == *"_levelset.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+echo "It's there!"
+echo "${array2[6]}"
+filename2=$(basename ${url2})
+call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+greyfile=${dir_to_save}/${filename2}
+echo "${greyfile}"
+fi
+if [[ ${url2} == *"_levelset.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+echo "It's there!"
+echo "${array2[6]}"
+filename2=$(basename ${url2})
+call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+greyfile=${dir_to_save}/${filename2}
+echo "${greyfile}"
+fi
+if [[ ${url2} == *"_levelset_bet.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+echo "It's there!"
+echo "${array2[6]}"
+filename2=$(basename ${url2})
+call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+betfile=${dir_to_save}/${filename2}
+echo "${betfile}"
+fi
+if [[ ${url2} == *"_csf_unet.nii.gz"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+echo "It's there!"
+echo "${array2[6]}"
+filename2=$(basename ${url2})
+call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+csffile=${dir_to_save}/${filename2}
+echo "${csffile}"
+fi
+done < <(tail -n +2 "${working_dir}/${output_csvfile_1}")
+
+
+##############################################
+resource_dir="PREPROCESS_SEGM_3"
+output_csvfile_2=${sessionID}_PREPROCESS_SEGM_METADATA.csv
+call_get_resourcefiles_metadata_saveascsv_args ${url1} ${resource_dir} ${working_dir} ${output_csvfile_2}
+#      filename1=$(basename ${url1})
+#  call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url1} ${filename1} ${dir_to_save})
+#  outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+echo "csffile:::::ATUL:::${csffile}"
+while IFS=',' read -ra array2; do
+url2=${array2[6]}
+
+#          if [[ ${url2} == *"ventricle_bounds.csv"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+#            echo "It's there!"
+#            echo "${array2[6]}"
+#            filename2=$(basename ${url2})
+#            call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+#            outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+#            ventricleboundfile=${dir_to_save}/${filename2}
+#            echo "${ventricleboundfile}"
+#          fi
+if [[ ${url2} == *"warped_1_mov_VENTRICLE_COLIHM62"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+echo "It's there!"
+echo "${array2[6]}"
+filename2=$(basename ${url2})
+call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+venticle_only_mask=${dir_to_save}/${filename2}
+echo "${venticle_only_mask}"
+fi
+
+if [[ ${url2} == *"warped_1_mov_CISTERN_COLIHM62"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+echo "It's there!"
+echo "${array2[6]}"
+filename2=$(basename ${url2})
+call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+cistern_only_mask=${dir_to_save}/${filename2}
+echo "${cistern_only_mask}"
+fi
+
+if [[ ${url2} == *"warped_1_mov_CISTERN_COLIHM62"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+echo "It's there!"
+echo "${array2[6]}"
+filename2=$(basename ${url2})
+call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+midline_only_mask=${dir_to_save}/${filename2}
+echo "${midline_only_mask}"
+fi
+
+done < <(tail -n +2 "${working_dir}/${output_csvfile_2}")
+#        venticle_only_mask=${betfile}
+#        echo "${venticle_only_mask} ${csffile} ${dir_to_save} ${greyfile} ${betfile}"
+#        python3 findventriclemaskconvexhull10112024.py  ${venticle_only_mask} ${csffile} ${dir_to_save} ${greyfile} ${betfile}
+ventricleboundfile=${dir_to_save}/'ventricle_bounds.csv'
+echo "python3 findventriclemaskobb_10102024.py  ${venticle_only_mask} ${csffile} ${dir_to_save} ${greyfile} ${betfile}"
+python3 findventriclemaskobb_10102024.py  ${venticle_only_mask} ${csffile} ${dir_to_save} ${greyfile} ${betfile}
 #
-#  while IFS=',' read -ra array1; do
-#    url1=${array1[0]}
-#    output_csvfile_1="${sessionID}_MASK_METADATA.csv"
-#    call_get_resourcefiles_metadata_saveascsv_args ${url1} MASKS ${working_dir} ${output_csvfile_1}
-#
-#    scanID=$(get_scanID_from_sessionID ${sessionID} ${working_dir})
-#
-#    function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} MASKS '_ventricle')
-#    outputfiles_present=$(python3 download_with_session_ID.py "${function_with_arguments[@]}")
-#    function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} MASKS '_total')
-#    outputfiles_present=$(python3 download_with_session_ID.py "${function_with_arguments[@]}")
-#
-#    while IFS=',' read -ra array2; do
-#      url2=${array2[6]}
-#      filename2=$(basename ${url2})
-#
-#      if [[ ${url2} == *"_vertical_bounding_box_512x512.nii.gz"* ]]; then
-#        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
-#        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#        greyfile=${dir_to_save}/${filename2}
-#      fi
-#
-#      if [[ ${url2} == *"_levelset.nii.gz"* ]]; then
-#        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
-#        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#        greyfile=${dir_to_save}/${filename2}
-#      fi
-#
-#      if [[ ${url2} == *"_levelset_bet.nii.gz"* ]]; then
-#        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
-#        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#        betfile=${dir_to_save}/${filename2}
-#      fi
-#
-#      if [[ ${url2} == *"_csf_unet.nii.gz"* ]]; then
-#        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
-#        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#        csffile=${dir_to_save}/${filename2}
-#      fi
-#    done < <(tail -n +2 "${working_dir}/${output_csvfile_1}")
-#
-#    # Download PREPROCESS_SEGM_3 masks
-#    output_csvfile_2="${sessionID}_PREPROCESS_SEGM_METADATA.csv"
-#    call_get_resourcefiles_metadata_saveascsv_args ${url1} PREPROCESS_SEGM_3 ${working_dir} ${output_csvfile_2}
-#
-#    while IFS=',' read -ra array2; do
-#      url2=${array2[6]}
-#      filename2=$(basename ${url2})
-#
-#      if [[ ${url2} == *"warped_1_mov_VENTRICLE_COLIHM62"* ]]; then
-#        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
-#        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#        venticle_only_mask=${dir_to_save}/${filename2}
-#      fi
-#
-#      if [[ ${url2} == *"warped_1_mov_CISTERN_COLIHM62"* ]]; then
-#        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
-#        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-#        cistern_only_mask=${dir_to_save}/${filename2}
-#        midline_only_mask=${dir_to_save}/${filename2}
-#      fi
-#    done < <(tail -n +2 "${working_dir}/${output_csvfile_2}")
-#
-#    ventricleboundfile=${dir_to_save}/'ventricle_bounds.csv'
-#    python3 findventriclemaskobb_10102024.py ${venticle_only_mask} ${csffile} ${dir_to_save} ${greyfile} ${betfile}
-#    python3 findventriclemaskobb_03102025.py ${cistern_only_mask} ${csffile} ${dir_to_save} ${greyfile} ${betfile}
-#
-#    while IFS=',' read -ra array3; do
-#      zoneV_min_z=${array3[3]}
-#      zoneV_max_z=${array3[4]}
-#    done < <(tail -n +2 "${ventricleboundfile}")
-#
-#    ventricle_after_deepreg=${dir_to_save}/ventricle.nii
-#    cistern_after_deepreg=${dir_to_save}/cistern_after_deepreg.nii
-#
-#    call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem_with_cis_1' ${greyfile} ${csffile} ${ventricle_after_deepreg} ${cistern_after_deepreg})
-#    outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
-#
-#    URI_1=${url2%/resource*}
-#    filename_prefix=$(basename ${url})
-#    filename_prefix=${filename_prefix%_NIFTILOCATION*}
-#    this_data_basename_noext=$(basename ${greyfile})
-#    this_data_basename_noext=${this_data_basename_noext%_resaved*}
-#
-#    for file_name in ${dir_to_save}/${filename_prefix}*.nii.gz; do
-#      if [[ ${file_name} == *"${this_data_basename_noext}"* ]] || [[ ${file_name} == *"ventricle"* ]] || [[ ${file_name} == *"sulci"* ]]; then
-#        call_uploadsinglefile_with_URI_arguments=('call_uploadsinglefile_with_URI' ${URI_1} ${file_name} MASKS)
-#        outputfiles_present=$(python3 /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
-#      fi
+echo "python3 findventriclemaskobb_03102025.py  ${cistern_only_mask} ${csffile} ${dir_to_save} ${greyfile} ${betfile}"
+python3 findventriclemaskobb_03102025.py  ${cistern_only_mask} ${csffile} ${dir_to_save} ${greyfile} ${betfile}
+ventricle_obb_mask=${dir_to_save}/ventricle_obb_mask.nii
+ventricle_after_deepreg=${dir_to_save}/ventricle.nii
+cistern_after_deepreg=${dir_to_save}/cistern_after_deepreg.nii
+while IFS=',' read -ra array3; do
+echo "${array3[3]}::${array3[4]}"
+zoneV_min_z=${array3[3]}
+zoneV_max_z=${array3[4]}
+done < <(tail -n +2 "${ventricleboundfile}")
+#############################################
+
+############
+
 #    done
-#  done < <(tail -n +2 "${dir_to_save}/${filename}")
-#done < <(tail -n +2 "${working_dir}/${output_csvfile}")
-#
-#echo ">>> DONE"
+
+
+################
+
+#echo "call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )"
+#call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )
+#outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
+##  echo ${outputfiles_present}
+#fi
+echo "call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem_with_cis_1' ${greyfile} ${csffile} ${ventricle_after_deepreg} ${cistern_after_deepreg})"
+#call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem_with_cis_1' ${greyfile} ${csffile} ${betfile} ${ventricle_obb_mask} ${zoneV_min_z} ${zoneV_max_z} )
+#exit 1
+call_csf_compartments_arguments=('call_csf_compartments_ventbound_no_hem_with_cis_1' ${greyfile} ${csffile}  ${ventricle_after_deepreg} ${cistern_after_deepreg} )
+outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
+
+
+#outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
+echo ${outputfiles_present}
+URI_1=${url2%/resource*}
+filename_prefix=$(basename ${url}) #${url2%/resource*} #filename=
+filename_prefix=${filename_prefix%_NIFTILOCATION*}
+resource_dirname="MASKS"
+this_data_basename=$(basename {greyfile})
+this_data_basename_noext=${this_data_basename%_resaved*}
+for file_name in ${dir_to_save}/${filename_prefix}*.nii.gz; do
+echo ${file_name}
+if [[ ${file_name} == *"${this_data_basename_noext}"* ]] || [[ ${file_name} == *"ventricle"* ]] || [[ ${file_name} == *"sulci"* ]]; then
+call_uploadsinglefile_with_URI_arguments=('call_uploadsinglefile_with_URI' ${URI_1} ${file_name} ${resource_dirname})
+outputfiles_present=$(python3 /software/download_with_session_ID.py "${call_uploadsinglefile_with_URI_arguments[@]}")
+echo ${outputfiles_present}
+fi
+done
+done < <(tail -n +2 "${dir_to_save}/${filename}")
+
+done \
+< \
+<(tail -n +2 "${working_dir}/${output_csvfile}")
