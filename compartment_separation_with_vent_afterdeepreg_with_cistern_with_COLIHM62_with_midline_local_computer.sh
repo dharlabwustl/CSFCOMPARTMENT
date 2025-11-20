42d23a18376e9516f04c673a511028a37bb05468
dharlabwustl <atulkumar@wustl.edu>
2025-11-20 10:40:14 -0600

    subsequent  Command fits

--- compartment_separation_with_vent_afterdeepreg_with_cistern_with_COLIHM62_with_midline_local_computer.sh
+++ compartment_separation_with_vent_afterdeepreg_with_cistern_with_COLIHM62_with_midline_local_computer.sh
@@ -159,10 +159,15 @@ call_csf_compartments_arguments=(
   "${cistern_after_deepreg}"
   "${output_directory}"
 )
+ echo  greyfile::"${greyfile}"
+ echo  csfile::"${csffile}"
+  echo ventricle_after_deepreg::"${ventricle_after_deepreg}"
+  echo cistern_after_deepreg::"${cistern_after_deepreg}"
+  echo output_directory::"${output_directory}"
 
-outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
-
-echo ">>> CSF compartment script output:"
-echo "${outputfiles_present}"
-
-echo ">>> DONE: COMPARTMENT_SEPARATION_WITH_VENT_BOUNDGIVEN_LOCAL.sh"
+#outputfiles_present=$(python3 /software/CSF_COMPARTMENT_GITHUB_July212023.py "${call_csf_compartments_arguments[@]}")
+#
+#echo ">>> CSF compartment script output:"
+#echo "${outputfiles_present}"
+#
+#echo ">>> DONE: COMPARTMENT_SEPARATION_WITH_VENT_BOUNDGIVEN_LOCAL.sh"
