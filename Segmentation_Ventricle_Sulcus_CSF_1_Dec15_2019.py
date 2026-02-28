53b00994d5b8916d13932b015f7aa01d87f7b8f6
sharmaatul11@gmail.com <your@email.com>
2026-02-28 14:39:51 -0600

    Commit Feb 2026

--- Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
+++ Segmentation_Ventricle_Sulcus_CSF_1_Dec15_2019.py
@@ -174,7 +174,7 @@ def divideintozones_upper_bound_given(filename_gray,filename_mask,filename_bet,u
             seg_explicit_thresholds = sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
 
             zoneV_min_z,zoneV_max_z=get_ventricles_range(sitk.GetArrayFromImage(seg_explicit_thresholds))
-            zoneV_max_z=upper_bound_slice_num
+            zoneV_max_z=upper_bound_slice_num-1
             subtracted_image=subtract_binary_1(sitk.GetArrayFromImage(img_T1_Copy),sitk.GetArrayFromImage(seg_explicit_thresholds)*255)
             subtracted_image=sitk.GetImageFromArray(subtracted_image)
             above_ventricle_image= sitk.GetArrayFromImage(subtracted_image)
@@ -296,7 +296,7 @@ def divideintozones_v1(filename_gray,filename_mask,filename_bet):
             seg_explicit_thresholds = sitk.ConnectedThreshold(img_T1, seedList=initial_seed_point_indexes, lower=100, upper=255)
 
             zoneV_min_z,zoneV_max_z=get_ventricles_range(sitk.GetArrayFromImage(seg_explicit_thresholds))
-            zoneV_max_z=37-1
+            # zoneV_max_z=37-1
             subtracted_image=subtract_binary_1(sitk.GetArrayFromImage(img_T1_Copy),sitk.GetArrayFromImage(seg_explicit_thresholds)*255)
             subtracted_image=sitk.GetImageFromArray(subtracted_image)
             above_ventricle_image= sitk.GetArrayFromImage(subtracted_image)
